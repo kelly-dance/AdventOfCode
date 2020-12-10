c5a91824321c3c744c7605595d67ea0822cf8ab5
McPqndq <kellybob3737@gmail.com>
2020-12-10 14:25:32 -0600

    day 10 part 2 alt solution

--- 10.js
+++ 10.js
@@ -1,21 +1,18 @@
-import { memoize, readFile } from './tools.ts';
+import { memoize, product, readFile, zip } from './tools.ts';
 
-const input = readFile('inputs/10.txt').split('\r\n').map(Number).sort((a,b)=>a-b);
+const input = readFile('inputs/10.txt').split('\r\n').map(Number).sort((a, b) => a - b);
+input.unshift(0);
 input.push(input[input.length - 1] + 3);
 
 let ones = 0;
 let threes = 0;
 
-let prev = 0;
-for(const next of input){
-  if(next - prev === 1) ones++;
-  if(next - prev === 3) threes++;
-  prev = next;
+for(const [prev, curr] of zip(input, input.slice(1))){
+  if(curr - prev === 1) ones++;
+  if(curr - prev === 3) threes++;
 }
 
 console.log(ones * threes);
-
-input.unshift(0);
 
 const from = memoize(index => {
   if(index === input.length - 1) return 1;
@@ -24,4 +21,22 @@ const from = memoize(index => {
   for(let i = index + 1; input[i] - n <= 3 && i < input.length; i++) score += from(i);
   return score;
 });
-console.log(from(0))
+
+console.log(from(0));
+
+// Wack code based on https://oeis.org/A000073
+const a = (19+3*Math.sqrt(33))**(1/3);
+const b = (19-3*Math.sqrt(33))**(1/3);
+const formula = n => Math.round(3*((a+b+1)/3)**(n+1)/(a**2+b**2+4));
+
+const straights = [];
+let straight = 1;
+for(const [prev, curr] of zip(input, input.slice(1))){
+  if(curr - prev === 1) straight++;
+  else { // curr - prev === 3 
+    straights.push(straight)
+    straight = 1;
+  }
+}
+
+console.log(product(straights.map(formula)))
