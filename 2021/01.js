6a1c3fe60d39bee946b4acb87e994671eb61c2e8
McPqndq <kellybob3737@gmail.com>
2021-12-01 01:02:09 -0700

    cleaned up 2021 day 1

--- 2021/01.js
+++ 2021/01.js
@@ -1,20 +1,8 @@
-import { readFile } from '../tools.ts';
+import { countMatches, readFile, sum, zip, zipWith  } from '../tools.ts';
 
-const input = readFile('./inputs/01.txt').split('\n').map(s => Number(s));
+const depths = readFile('./inputs/01.txt').split('\n').map(s => Number(s));
 
-let prev = input[0]
-let count = 0;
-for(const val of input.slice(1)){
-  if(val > prev) count++;
-  prev = val;
-}
-console.log(count)
+console.log(countMatches(zip(depths, depths.slice(1)), ([a, b]) => b > a));
 
-let count2 = 0;
-let prev2 = input[0] + input[1] + input[2];
-for(let i = 3; i < input.length; i++){
-  const newVal = prev2 - input[i-3] + input[i];
-  if(newVal > prev2) count2++;
-  prev2 = newVal;
-}
-console.log(count2)
+const windowed = zipWith(sum, depths, depths.slice(1), depths.slice(2))
+console.log(countMatches(zip(windowed, windowed.slice(1)), ([a, b]) => b > a));
