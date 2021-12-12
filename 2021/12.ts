33c76f8ef0e3591b2a5e15cbd3c9c17f5d182020
McPqndq <kellybob3737@gmail.com>
2021-12-12 15:44:19 -0700

    2021 day 12 optimization

--- 2021/12.ts
+++ 2021/12.ts
@@ -1,39 +1,49 @@
-import { DefaultMap, readAdvent, SortedSet } from '../tools.ts';
+import { DefaultMap, readAdvent } from '../tools.ts';
 
 const inp = (await readAdvent()).split('\n').map(s => s.split('-'));
 
-const cons = new DefaultMap<string, string[]>(() => []);
-for(const [t,f] of inp){
-  cons.get(t).push(f);
-  cons.get(f).push(t);
+const isLowerMap = new Map<number, boolean>();
+const labelToBin = new Map<string, number>();
+for(const pair of inp){
+  for(const label of pair){
+    if(labelToBin.has(label)) continue;
+    const value = 1 << labelToBin.size;
+    labelToBin.set(label, value);
+    isLowerMap.set(value, label === label.toLowerCase());
+  }
 }
 
-const cache = new Map<string, number>()
-const searchFrom = (current: string, visited: SortedSet<string>, part2: boolean): number => {
-  if(current === 'end') return 1;
-  const set = visited.reduce((a, c) => `${a},${c}`, '');
-  const key = `${current}:${set}:${part2}`;
+const cons = new DefaultMap<number, number[]>(() => []);
+for(const [t,f] of inp){
+  cons.get(labelToBin.get(t)!).push(labelToBin.get(f)!);
+  cons.get(labelToBin.get(f)!).push(labelToBin.get(t)!);
+}
+
+let start = labelToBin.get('start')!;
+let end = labelToBin.get('end')!;
+const cache = new Map<number, number>()
+const searchFrom = (current: number, visited: number, part2: boolean): number => {
+  if(current === end) return 1;
+  let key = (visited << 1) | (current << labelToBin.size) | Number(part2);
   if(cache.has(key)) return cache.get(key)!;
   let paths = 0;
   for(const path of cons.get(current)){
     let notDoubled = part2;
-    if(visited.has(path)){
-      if(part2 && path !== 'start') notDoubled = false;
+    if(visited & path){
+      if(part2 && path !== start) notDoubled = false;
       else continue;
     }
-    const changeSet = path === path.toLowerCase() && !(notDoubled !== part2);
-    if(changeSet) visited.add(path);
+    const changeSet = isLowerMap.get(path)! && !(notDoubled !== part2);
+    if(changeSet) visited |= path;
     paths += searchFrom(path, visited, notDoubled);
-    if(changeSet) visited.remove(path);
+    if(changeSet) visited &= ~path;
   }
   cache.set(key, paths);
   return paths;
 };
 
-let start = performance.now();
-const set = new SortedSet<string>((a,b) => a === b ? 0 : a < b ? -1 : 1);
-set.add('start');
-console.log(searchFrom('start', set, false)); // part 1
-console.log(searchFrom('start', set, true)); // part 2
-console.log(performance.now() - start, 'ms');
+let startTime = performance.now();
+console.log(searchFrom(start, start, false)); // part 1
+console.log(searchFrom(start, start, true)); // part 2
+console.log(performance.now() - startTime, 'ms');
 
