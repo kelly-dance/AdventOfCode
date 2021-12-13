6efa17769f8a66d509752703fd670f4b3c379e3a
McPqndq <kellybob3737@gmail.com>
2021-12-12 22:52:54 -0700

    2021 day 13 cleaned up a tiny bit

--- 2021/13.js
+++ 2021/13.js
@@ -3,26 +3,26 @@ import { printChar, readAdvent } from '../tools.ts';
 let [pairs, folds] = (await readAdvent()).split('\n\n')
 pairs = pairs.split('\n').map(s => s.split(',').map(n => parseInt(n)));
 folds = folds.split('\n').map(l => {
-  const [w,n] = l.split(' ')[2].split('=');
+  const [w, n] = l.split(' ')[2].split('=');
   return [w, parseInt(n)];
 });
 
 const fold = (pts, w, n) => {
   const newPts = new Map();
   if(w === 'x'){
-    for(const [x,y] of pts.values()){
-      if(x < n) newPts.set(`${x},${y}`,[x,y]);
+    for(const [x, y] of pts.values()){
+      if(x < n) newPts.set(`${x},${y}`,[x, y]);
       else {
-        const nx = 2*n - x;
-        newPts.set(`${nx},${y}`,[nx,y]);
+        const nx = 2 * n - x;
+        newPts.set(`${nx},${y}`, [nx, y]);
       }
     }
   } else {
-    for(const [x,y] of pts.values()){
-      if(y < n) newPts.set(`${x},${y}`,[x,y]);
+    for(const [x ,y] of pts.values()){
+      if(y < n) newPts.set(`${x},${y}`, [x, y]);
       else {
-        const ny = 2*n - y;
-        newPts.set(`${x},${ny}`,[x,ny]);
+        const ny = 2 * n - y;
+        newPts.set(`${x},${ny}`, [x, ny]);
       }
     }
   }
@@ -32,7 +32,7 @@ const fold = (pts, w, n) => {
 
 let pts = new Map();
 for(const [x, y] of pairs){
-  pts.set(`${x},${y}`, [x,y]);
+  pts.set(`${x},${y}`, [x, y]);
 }
 
 pts = fold(pts, ...folds[0]);
